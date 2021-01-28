ad291ceac37822849adf2971e97983595afde4c6
mziegler611 <57671001+mziegler611@users.noreply.github.com>
2021-01-28 14:29:48 -0500

    Added Withdraw and Balance Tests

--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -9,8 +9,19 @@ class BankAccountTest {
     @Test
     void getBalanceTest() {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
-
         assertEquals(200, bankAccount.getBalance());
+
+        //Balance is negative (should set it to zero)
+        BankAccount bankAccount2 = new BankAccount("a@b.com", -1);
+        assertEquals(0, bankAccount2.getBalance());
+
+        //Balance with a Double
+        BankAccount bankAccount3 = new BankAccount("a@b.com", 10.21);
+        assertEquals(10.21, bankAccount3.getBalance());
+
+        //Balance with a Double with too many places
+        BankAccount bankAccount4 = new BankAccount("a@b.com", 10.213);
+        assertEquals(10.21, bankAccount4.getBalance());
     }
 
     @Test
@@ -20,6 +31,27 @@ class BankAccountTest {
 
         assertEquals(100, bankAccount.getBalance());
         assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300));
+
+        //Withdraw a negative amount
+        bankAccount.withdraw(-100);
+        assertEquals(100, bankAccount.getBalance());
+        
+        //Withdraw 0
+        bankAccount.withdraw(0);
+        assertEquals(100, bankAccount.getBalance());
+
+        //Edge Case
+        bankAccount.withdraw(.000000000001);
+        assertEquals(100, bankAccount.getBalance());
+
+        //Withdraw a Regular Double 
+        bankAccount.withdraw(0.01);
+        assertEquals(99.99, bankAccount.getBalance());
+
+        //Withdraw a Double with too many places
+        bankAccount.withdraw(0.999);
+        assertEquals(99.00, bankAccount.getBalance());
+
     }
 
     @Test
