503b15eac73b64c6b2a312ab5c58c5dd4f195e32
Morgan <morgan.e.ziegler@gmail.com>
2021-02-02 13:16:47 -0500

    Added constructor tests, transfer, and deposit

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -7,11 +7,17 @@ public class BankAccount {
 
     /**
      * @throws IllegalArgumentException if email is invalid
+     * @throws IllegalArgumentException if amount is invalid
      */
     public BankAccount(String email, double startingBalance){
         if (isEmailValid(email)){
             this.email = email;
-            this.balance = startingBalance;
+            if (isAmountValid(startingBalance)){
+                this.balance = startingBalance;
+            }
+            else{
+                throw new IllegalArgumentException("Starting Balance: " + startingBalance + " is invalid, cannot create account");
+            }
         }
         else {
             throw new IllegalArgumentException("Email address: " + email + " is invalid, cannot create account");
@@ -32,13 +38,12 @@ public class BankAccount {
     }
 
     /**
-     * @post reduces the balance by amount if amount is non-negative and smaller than balance
+     * @post reduces the balance by amount if amount is non-negative and smaller than balance, if it is negative throw error
+     * @throws IllegalArgumentException if amount is invalid
      */
     public void withdraw (double amount) throws InsufficientFundsException{
         
-        if(amount > 0){
-            int num  = (int)(amount * 100);
-            amount = ((double)num) /100; //two decimal places
+        if(isAmountValid(amount)){
             if (amount <= balance){
                 balance -= amount;
             }
@@ -46,10 +51,75 @@ public class BankAccount {
                 throw new InsufficientFundsException("Not enough money");
             }
         }
+        else{
+            throw new IllegalArgumentException("Invalid money value");
+        }
+       
+        
     }
     
 
+    /**
+     * @post adds to the balance by amount if amount is non-negative and smaller than balance, if it is negative throw error
+     * @throws IllegalArgumentException if amount is invalid
+     */
+    public void deposit (double amount) throws IllegalArgumentException{
+        if(isAmountValid(amount)){
+            balance += amount;
+        }
+        else{
+            throw new IllegalArgumentException("Invalid money value");
+        }
+    }
 
+
+    /*
+    * Checks if the double is positive and only has two decimal places
+    */
+    public static boolean isAmountValid(double amount){
+        if(amount >= 0){
+            int num  = (int)(amount * 100);
+            double roundedAmt = ((double)num) /100; //two decimal places
+            if (roundedAmt - amount == 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    /**
+     * @post checks if it can withdraw the money from the first account
+     * Then deposits it in the second account if valid
+     * @throws InsufficientFundsException if amount is larger than the first accs balance
+     * @throws IllegalArgumentException if amount is invalid
+     */
+    public static void transfer(BankAccount toWithdraw, BankAccount toDeposit, double amount)
+            throws InsufficientFundsException {
+        if(isAmountValid(amount)){
+            try{ 
+                toWithdraw.withdraw(amount);
+            }
+            catch(InsufficientFundsException e){
+                throw new InsufficientFundsException("not enough money");
+            }
+            toDeposit.deposit(amount);
+        }
+        else{
+            throw new IllegalArgumentException("Invalid amount");
+        }
+    }
+
+    /**
+     * given a string determines if that email is valid or not
+     * @param email
+     * @return true/false depending if the email is valid
+     */
     public static boolean isEmailValid(String email){
         if (email.indexOf('@') == -1 || email.indexOf(".com") == -1){
             return false;
@@ -77,3 +147,4 @@ public class BankAccount {
         return true;
     }
 }
+
--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -33,25 +33,23 @@ class BankAccountTest {
         assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300)); //Overdrawing
 
         //Withdraw a negative amount
-        bankAccount.withdraw(-100);
-        assertEquals(100, bankAccount.getBalance()); //Equivalence class Negative amount
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.withdraw(-100));
+        //Equivalence class Negative amount
         
         //Withdraw 0
         bankAccount.withdraw(0);
         assertEquals(100, bankAccount.getBalance()); //Equivalence class withdrawing nothing
 
-        //Edge Case
-        bankAccount.withdraw(.000000000001);
-        assertEquals(100, bankAccount.getBalance()); //Edge Case for any number not in the correct format
+        
+        //Edge Case for any number not in the correct format
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.withdraw(.00000000001));
 
         //Withdraw a Regular Double 
         bankAccount.withdraw(0.01);
         assertEquals(99.99, bankAccount.getBalance());  //Equivalence class
 
         //Withdraw a Double with too many places
-        bankAccount.withdraw(0.999);
-        assertEquals(99.00, bankAccount.getBalance()); //Equivalence class 
-
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.withdraw(.999));
     }
 
     @Test
@@ -83,6 +81,24 @@ class BankAccountTest {
     }
 
     @Test
+    void isAmountValidTest(){
+        
+
+        //Valid Inputs
+        assertTrue(BankAccount.isAmountValid(10));
+        assertTrue(BankAccount.isAmountValid(10.0));
+        assertTrue(BankAccount.isAmountValid(10.01));
+
+        //Invalid edge cases
+        assertFalse(BankAccount.isAmountValid(10.011));
+        assertFalse(BankAccount.isAmountValid(-0.01));
+
+        //Negative Inputs
+        assertFalse(BankAccount.isAmountValid(-10));
+        assertFalse(BankAccount.isAmountValid(-10.00));
+    }
+
+    @Test
     void constructorTest() {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
 
@@ -90,6 +106,74 @@ class BankAccountTest {
         assertEquals(200, bankAccount.getBalance());
         //check for exception thrown correctly
         assertThrows(IllegalArgumentException.class, ()-> new BankAccount("", 100));
+
+    
+        //Equivalence class any negative amount
+        assertThrows(IllegalArgumentException.class, ()-> new BankAccount("", -1));
+
+        //Balance with a Double
+        BankAccount bankAccount3 = new BankAccount("a@b.com", 10.21); //Equivalence class
+        assertEquals("a@b.com", bankAccount3.getEmail());
+        assertEquals(10.21, bankAccount3.getBalance());
+
+        //Balance with a Double with too many places
+        assertThrows(IllegalArgumentException.class, ()-> new BankAccount("", 10.213));
     }
+
+    @Test
+    void depositTest() throws InsufficientFundsException{
+        BankAccount bankAccount = new BankAccount("a@b.com", 200);
+        bankAccount.deposit(100);
+
+        assertEquals(300, bankAccount.getBalance()); //Correctly amount left from deposit
+       
+
+        //Deposit a negative amount
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(-100));
+        //Equivalence class Negative amount
+        
+        //Deposit 0
+        bankAccount.deposit(0);
+        assertEquals(300, bankAccount.getBalance()); //Equivalence class deposit nothing
+
+        
+        //Edge Case for any number not in the correct format
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(.00000000001));
+
+        //Deposit a Regular Double 
+        bankAccount.deposit(0.01);
+        assertEquals(300.01, bankAccount.getBalance());  //Equivalence class
+
+        //Depost a Double with too many places
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(.999));
+    }
+
+    @Test
+    void transferTest() throws InsufficientFundsException{
+        BankAccount bankAccount = new BankAccount("a@b.com", 200);
+        BankAccount bankAccount2 = new BankAccount("a@b.com", 200);
+        
+
+       //Transfer a normal amount
+       BankAccount.transfer(bankAccount, bankAccount2, 100);
+       assertEquals(100, bankAccount.getBalance()); 
+       assertEquals(300, bankAccount2.getBalance()); 
+
+       //Transfer a number with too many places
+       assertThrows(IllegalArgumentException.class, () -> BankAccount.transfer(bankAccount, bankAccount2, 10.001));
+
+       //Transfer a negative number
+       assertThrows(IllegalArgumentException.class, () -> BankAccount.transfer(bankAccount, bankAccount2, -1));
+
+        //Transfer a value larger than the account balance to withdraw from
+       assertThrows(InsufficientFundsException.class, () -> BankAccount.transfer(bankAccount, bankAccount2, 500));
+        
+       //Transfer a normal amount with double
+       BankAccount.transfer(bankAccount, bankAccount2, 50.50);
+       assertEquals(49.50, bankAccount.getBalance()); 
+       assertEquals(350.50, bankAccount2.getBalance()); 
+    }
+
+    
 
 }
